11180a6e256e31bed359dc1f5c4ae314d33d9074
Muhammad Rizqullah Akbar <muhrizqullah@icloud.com>
2022-10-14 14:15:40 +0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Muhammad Rizqullah Akbar on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
